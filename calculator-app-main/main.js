5df6880931500478674011d1d1eb0d0a6ee07bb6
FelixDot <felix.tran002@gmail.com>
2023-08-10 09:11:17 +0200

    added main.js for calc app todo:     bug with operator "*" can be used to add and display mulitple operators     use semantic html     fix display-length     add mobile design

--- calculator-app-main/main.js
+++ calculator-app-main/main.js
@@ -5,6 +5,9 @@ document.addEventListener("DOMContentLoaded", function () {
   const theme3 = document.getElementById("theme-3");
   const body = document.getElementById("theme");
 
+  const display = document.getElementById("display");
+  const buttons = document.getElementsByClassName("key");
+
   function toggleTheme(radio) {
     if (radio.checked) {
       body.className = radio.value;
@@ -15,4 +18,68 @@ document.addEventListener("DOMContentLoaded", function () {
   radios.forEach((radio) =>
     radio.addEventListener("click", () => toggleTheme(radio))
   );
+
+  function clearScreen() {
+    document.getElementById("display").textContent = "";
+  }
+
+  document.getElementById("reset").addEventListener("click", clearScreen);
+
+  let input = "";
+
+  document.querySelectorAll(".number, .operator").forEach((button) => {
+    button.addEventListener("click", () => {
+      const value = button.textContent;
+
+      if (value === "." && input.includes(".")) {
+        return;
+      }
+      if (value === "0" && input === "0") {
+        return;
+      }
+      if (
+        input.length > 0 &&
+        isOperator(value) &&
+        isOperator(input[input.length - 1])
+      ) {
+        input = input.slice(0, input.length - 1);
+      }
+
+      input += value === "x" ? "*" : value;
+      display.innerText = input;
+    });
+  });
+
+  document.getElementById("equals").addEventListener("click", () => {
+    if (input.length === 0) {
+      return;
+    }
+
+    try {
+      let result = eval(input);
+      display.innerText = result;
+      input = result.toString();
+    } catch (error) {
+      display.innerText = "Error";
+      input = "";
+    }
+  });
+
+  document.getElementById("reset").addEventListener("click", () => {
+    input = "";
+    display.innerText = "";
+  });
+
+  document.getElementById("delete").addEventListener("click", () => {
+    input = input.slice(0, input.length - 1);
+    display.innerText = input;
+  });
+
+  document.getElementById("value").addEventListener("click", () => {
+    console.log(input);
+  });
+
+  function isOperator(value) {
+    return ["+", "-", "*", "/"].includes(value);
+  }
 });
